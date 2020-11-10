31aa6a1a6c772835a7ed22a719a54d6d2ad2ff3d
Krzesimir Modelski <k.modelski96@gmail.com>
2020-11-10 17:27:07 +0100

    fixed game typo

--- frontend/src/phaser/PhaserMain.js
+++ frontend/src/phaser/PhaserMain.js
@@ -129,7 +129,7 @@ const PhaserMain = () => {
         scoreText = this.add.text(16, 16, "Score: 0", {fontSize: '32px', color: 'black'});
         scoreText.setScrollFactor(0, 0)
         // restart text
-        this.add.text(580, 16, "To restart press: R", {fontSize: '32px', color: 'black'}).setScrollFactor(0, 0)
+        this.add.text(580, 16, "Press: \"R\" to restart", {fontSize: '32px', color: 'black'}).setScrollFactor(0, 0)
         // controls
         this.add.text(16, 600, "arrows - move", {fontSize: '32px', color: 'black'})
         this.add.text(16, 632, "space - attack", {fontSize: '32px', color: 'black'})
