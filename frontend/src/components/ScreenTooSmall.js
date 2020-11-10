f03ab323f3e27a22329bbc9a11d2c5232c0b18be
Krzesimir Modelski <55512362+Delp6@users.noreply.github.com>
2020-11-10 13:01:44 +0100

    Update and rename ScreenToSmall.js to ScreenTooSmall.js
    Fixed screen too small typo.

--- frontend/src/components/ScreenTooSmall.js
+++ frontend/src/components/ScreenTooSmall.js
@@ -7,7 +7,7 @@ export const ScreenToSmall = () => {
     return (
         <div style={{color: "white", fontSize: "3rem", textAlign: "center"}}>
             <DesktopAccessDisabledIcon style={{fontSize: "12rem"}}/>
-        <p>Sorry, your screen is to tinny.</p>
+        <p>Sorry, your screen is too tiny.</p>
     </div>
     )
 }
